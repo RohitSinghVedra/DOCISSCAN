f9433e97fed6c8603d5cba939cd49a23d079cc1c
Rohit Singh <risingh.official.work@gmail.com>
2025-10-27 03:55:01 +0400

    Switch to Firebase authentication components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './components/Login';
-import Register from './components/Register';
+import Login from './components/LoginFirebase';
+import Register from './components/RegisterFirebase';
 import Dashboard from './components/Dashboard';
 import CameraScan from './components/CameraScan';
 import DocumentList from './components/DocumentList';
