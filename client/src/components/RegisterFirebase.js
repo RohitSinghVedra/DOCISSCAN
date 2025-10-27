438d4b7d7871b264dd3855cad2a5e6033be3ce1b
Rohit Singh <risingh.official.work@gmail.com>
2025-10-27 13:09:47 +0400

    Remove unused Link import

--- client/src/components/RegisterFirebase.js
+++ client/src/components/RegisterFirebase.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { auth } from '../firebase-config';
 import { toast } from 'react-toastify';
